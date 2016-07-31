78b9c50e2f9dc2e9e4639a8aeec27a21647d04e6
Wesley Aspinall <wra427@gmail.com>
2016-07-31 18:33:35 -0400

    updated server.js

--- server.js
+++ server.js
@@ -4,13 +4,10 @@ var bodyParser = require('body-parser');
 
 
 var app = express();
-app.use(express.static(__dirname + "/app"));
+app.use(express.static(__dirname + '/app'));
 
-app.get('/hello', function(req, res){
-	res.send(JSON.stringify('hello, world'));
-})
 
 var server = app.listen(process.env.PORT || 8000, function () {
   var port = server.address().port;
-  console.log("App now running on port", port);
+  console.log('App now running on port', port);
 });
