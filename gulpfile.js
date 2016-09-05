960224f657eb7524a7c491a55f88d0464b355fd3
Wesley Aspinall <wra427@gmail.com>
2016-09-05 14:58:56 -0400

    updated tasks: sass(now includes pcss and autoprefixer) removed browserify-test from default tasks created individual development server tasks for client/server code refactored to es6 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,162 +1,180 @@
-var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var browserify = require('browserify');
-var babel = require('babelify');
-var sass = require('gulp-sass');
-var plumber = require('gulp-plumber');
-var notify  = require('gulp-notify');
-var uglify = require('gulp-uglify');
-var server  = require('gulp-server-livereload');
-var fontAwesome = require('node-font-awesome');
-var jshint = require('gulp-jshint');
-var stylish = require('jshint-stylish');
+const gulp = require('gulp');
+
+//streams
+const sourcemaps = require('gulp-sourcemaps');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
+
+// bundlers, watchers, etc.
+const babel = require('babelify');
+const browserify = require('browserify');
+const watch = require('gulp-watch');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+
+//css 
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+//font libraries
+const fontAwesome = require('node-font-awesome');
+
+//Bootstrap
+const path = require('path');
+const bootstrapEntry = require.resolve('bootstrap-sass');
+const bootstrapSCSS = path.join(bootstrapEntry, '..', '..', 'stylesheets');
+
+//localhost, node server
+const server = require('gulp-server-livereload');
+const nodemon = require('gulp-nodemon');
+
+//linters
+const eslint = require('gulp-eslint');
 var htmlhint = require('gulp-htmlhint');
-var jscs = require('gulp-jscs');
-var watch = require('gulp-watch'); // A Better File Watcher
-var nodemon = require('gulp-nodemon');
 
-// Set up Bootstrap
-var path = require('path');
-var bootstrapEntry = require.resolve('bootstrap-sass');
-var bootstrapSCSS = path.join(bootstrapEntry, '..', '..', 'stylesheets');
+//error handlers
+const notify = require('gulp-notify');
+const plumber = require('gulp-plumber');
 
-
-var notifyError = function() {
+const notifyError = () => {
   return plumber({
     errorHandler: notify.onError("Error: <%= error.message %>")
   });
 }
 
-var browserifyError = function(err) {
+const browserifyError = (err) => {
   notify.onError("Error: <%= error.message %>")(err);
   this.emit('end');
 }
 
-
-gulp.task('sass', function () {
+//////////////////////////
+// styles+fonts+linters// 
+////////////////////////
+gulp.task('sass', () => {
   gulp.src('./sass/main.scss')
-    .pipe( notifyError() )
-    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(notifyError())
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
     .pipe(sass({
       includePaths: require('node-neat').with([fontAwesome.scssPath, bootstrapSCSS])
     }))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('./app/css'));
 });
 
-gulp.task('fonts', function() {
+gulp.task('normalize', () => {
+  gulp.src(require.resolve('normalize.css'))
+    .pipe(notifyError())
+    .pipe(gulp.dest('./app/css'));
+});
+
+gulp.task('fonts', () => {
   gulp.src(fontAwesome.fonts)
-    .pipe( notifyError() )
+    .pipe(notifyError())
     .pipe(gulp.dest('./app/fonts'));
 });
 
-gulp.task('normalize', function() {
-  gulp.src(require.resolve('normalize.css'))
-    .pipe( notifyError() )
-    .pipe(gulp.dest('./app/css'));
+gulp.task('style:js', () => {
+  return gulp.src('./js/**/**.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
 });
 
-gulp.task('browserify', function() {
-  return browserify('./js/main.js', {debug: true})
-    .transform(babel)
-    .bundle()
-    .on('error', browserifyError)
-    .pipe(source('./main.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    // .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./app/js'));
-});
-
-// Janky - quick fix to write spec file
-gulp.task('browserify-test', function() {
-  return browserify('./js/tests.js', {debug: true})
-    .transform(babel)
-    .bundle()
-    .on('error', browserifyError)
-    .pipe(source('./tests.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true}))
-    // .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./spec/'));
-});
-
-gulp.task('style:js', function() {
-  return gulp.src('./js/**/*.js')
-    .pipe(notifyError())
-    .pipe(jscs())
-    .pipe(jscs.reporter())
-    .pipe(jscs.reporter('fail'))
-});
-
-gulp.task('hint:js', function() {
-  return gulp.src('./js/**/*.js')
-    .pipe(notifyError())
-    .pipe(jshint({
-      esnext: true, eqeqeq: true,
-      linter: require('jshint-jsx').JSXHINT
-    }))
-    .pipe(jshint.reporter('fail'))
-    .pipe(jshint.reporter('jshint-stylish'));
-});
-
-gulp.task('hint:html', function() {
+gulp.task('hint:html', () => {
   return gulp.src('./app/index.html')
     .pipe(notifyError())
     .pipe(htmlhint())
     .pipe(htmlhint.failReporter());
 });
 
-gulp.task('lint', ['style:js', 'hint:js', 'hint:html']);
 
-gulp.task('watch', function() {
-  watch('./sass/**/*.scss', function () {  
-    gulp.start('sass'); 
-  });
-  watch(['./js/**/*.js', './package.json'], function () {
-    gulp.start('browserify');
-    gulp.start('browserify-test');
-  });
-  watch('./app/index.html', function () {
-    gulp.start('hint:html');
-  });
-  watch('./js/**/*.js', function () {
-    gulp.start('hint:js');
-    gulp.start('style:js');
-  });
+///////////////
+// Bundlers //
+/////////////
+gulp.task('browserify', () => {
+  return browserify('./js/main.js', {
+      debug: true
+    })
+    .transform(babel)
+    .bundle()
+    .on('error', browserifyError)
+    .pipe(source('./main.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./app/js'));
 });
 
-gulp.task('server', ['default'], function () {
-  return gulp.src('app')
+
+gulp.task('browserify-test', () => {
+  return browserify('./js/tests.js', {
+      debug: true
+    })
+    .transform(babel)
+    .bundle()
+    .on('error', browserifyError)
+    .pipe(source('./tests.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({
+      loadMaps: true
+    }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./spec/'));
+});
+
+
+gulp.task('watch', () => {
+  watch('./sass/**/*.scss', () => gulp.start('sass'));
+  watch(['./js/**/*.js', './package.json'], () => gulp.start(['browserify', 'browserify-test']));
+  watch('./app/index.html', () => gulp.start('hint:html'));
+  watch('./js/**/*.js', () => gulp.start('style:js'));
+});
+
+//////////////////
+// server tasks//
+////////////////
+gulp.task('server', ['default'], () => {
+  gulp.src('app')
     .pipe(server({
-      livereload: true
-    }));
+      livereload: {
+        enable: true
+      },
+    }))
 });
 
-gulp.task('nodemon', function () {
-  var started = false;
+gulp.task('nodemon', () => {
+  gulp.src('server.js')
   var stream = nodemon({
-                script: 'server.js', // run ES5 code 
-               }).on('start', function () {
-    // to avoid nodemon being started multiple times
-    // thanks @matthisk
-    if (!started) {
-      cb();
-      started = true; 
-    } 
-  });
+    script: 'server.js'
+  })
   return stream;
-});
+})
 
-gulp.task('default', ['sass',
-                      'fonts',
-                      'normalize',
-                      'lint',
-                      'browserify',
-                      'browserify-test']);
+gulp.task('lint', ['style:js', 'hint:html']);
 
-gulp.task('start', ['default', 'watch', 'server', 'nodemon']);
+gulp.task('default', [
+  'sass',
+  'fonts',
+  'normalize',
+  'lint',
+  'browserify'
+]);
+
+
+//serve
+gulp.task('serve-client', ['default', 'watch', 'server']);
+
+gulp.task('serve-express', ['default', 'watch', 'nodemon']);
+
+//serve both client and server code
+gulp.task('start', ['default', 'watch','server','nodemon' ]);
+
+
+
+
+
